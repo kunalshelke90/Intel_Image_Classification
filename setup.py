b05a1108dcf5ed88af2a92ec1a2fb58346a532ed
kunalshelke90 <shelkekunal90@gmail.com>
2024-09-12 16:01:57 +0530

    Completed logger and exception file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Intel-Image-Classification-with-PyTorch",
+    version="0.0.1",
+    author="Kunal Shelke",
+    author_email="shelkekunal90@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
